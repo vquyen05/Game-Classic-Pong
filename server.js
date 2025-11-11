079fa5bd0f6973ac8aa02fd847d98cec7497d4f6
vquyen05 <quyenvv0522@ut.edu.vn>
2025-11-11 22:14:10 +0700

    Khởi tạo dự án Game Ping Pong classic

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname + "/public"));
