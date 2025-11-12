971438d327940e9c51625cd3f0582eafe50792ef
Loi1966 <tangloi2989@gmail.com>
2025-11-12 20:35:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(__dirname + "/public"));
+let rooms ={}; //{ roomId: { player: {}, ball: {},status: 'waiting'/'playing}}
+Const canvasWidth = 600;
+const canvasHeight = 400;
+const Win_SCORE = 5;// Tốc độ ban đầu của bóng
+const INITIAL_BALL-SPEED =5;
+const SPEED_MULTIPLIER = 1.05;// Hệ số nhân tốc độ sau mỗi lần chạm
+const MAX_BALL_SPEED = 30;// Giới hạn tốc độ tối đa của bóng
+
